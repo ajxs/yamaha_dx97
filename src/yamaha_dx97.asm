607e59a4d2327fba647872e97e1b877787f2dce7
Anthony <ajxs@panoptic.online>
2023-05-13 18:04:34 +1000

    Update ROM checksum remainder byte

--- src/yamaha_dx97.asm
+++ src/yamaha_dx97.asm
@@ -113,7 +113,7 @@
 ; then run the associated script to generate the remainder byte.
 ; Refer to the external documentation for the tools used to calculate this.
 checksum_remainder_byte:
-    DC.B #74
+    DC.B #5
 
     ORG $FFEE
 
