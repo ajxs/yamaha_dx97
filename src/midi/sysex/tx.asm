e1396269dd67ca0fe8a27e9bf63e57d24b92d5cc
Anthony <ajxs@panoptic.online>
2023-08-03 18:09:09 +1000

    Added license text to repository; Updated license header to use 'SPDX-License-Identifier'

--- src/midi/sysex/tx.asm
+++ src/midi/sysex/tx.asm
@@ -2,11 +2,7 @@
 ; YAMAHA DX9/7 FIRMWARE
 ; Copyright (C) 2022 AJXS (https://ajxs.me/)
 ;
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or
-; (at your option) any later version.
-;
+; SPDX-License-Identifier: GPL-3.0-or-later
 ; ==============================================================================
 ; midi/sysex/tx.asm
 ; ==============================================================================
